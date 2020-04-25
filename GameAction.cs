8db4ec25bcac4ae98e1e40ba14f2bf3b5e736c8b
ClementSurel <57773412+ClementSurel@users.noreply.github.com>
2020-04-25 15:26:54 +0200

    recreate GameAction class

---
+++ GameAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class GameAction
+{
+	private string name;
+	public delegate bool actionFunction ();
+	private actionFunction actionf ;
+	
+	public GameAction(string name, actionFunction f)
+	{
+		this.name = name;
+		actionf = f;
+	}
+
+	public bool execute ()
+	{
+		return actionf();
+	}
+}
